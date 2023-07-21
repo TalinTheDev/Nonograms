d4ee71941b2ce3973050035dce7dddb26b6c47e0
Talin Sharma <talinsharma.dev@gmail.com>
2023-07-21 16:28:32 -0400

    Version 1.0.0

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,78 +1,94 @@
 <script setup>
-import {reactive  } from 'vue';
-import {Timer} from '../helpers/timer.js'
+import { reactive } from 'vue';
+import { Timer } from '../helpers/timer.js'
+
 const props = defineProps(['size'])
 
-const rowCount = parseInt(props.size.split('x')[0])
-const columnCount = parseInt(props.size.split('x')[1])
-var notStarted = true;
-var matrix = Array(parseInt(props.size.split('x')[0])).fill(null).map(() => Array(parseInt(props.size.split('x')[1])).fill(0));
-var values = {
-  rows: new Array(parseInt(props.size.split('x')[0])).fill(0),
-  columns: new Array(parseInt(props.size.split('x')[1])).fill(0),
-}
-const timer = reactive(new Timer());
-var columns = [];
-for (var i = 0; i < columnCount; i++) {
-  columns.push([])
-}
+var matrix;
+var values;
+var timer;
+var columnsValues;
+var gameStarted;
+var rows = parseInt(props.size.split('x')[0]);
+var columns = parseInt(props.size.split('x')[1]);
 
-for (var i = 0; i < matrix.length; i++) {
-  var row = matrix[i];
-  for (var j = 0; j < row.length; j++) {
-    row[j] = (Math.floor(Math.random() * 24) + 1) % 2 != 0 ? true : false;
-    columns[j][i] = row[j]
+const init = (rows, columns) => {
+  /* INIT Values */
+  console.log(rows, columns)
+  matrix = Array(rows).fill(null).map(() => Array(columns));
+  values = reactive({
+    rows: new Array(rows).fill(0),
+    columns: new Array(columns).fill(0)
+  });
+  timer = reactive(new Timer());
+  gameStarted = false;
+  columnsValues = [];
+
+  for (var i = 0; i < columns; i++) { // Populate columnsValues with empty arrays
+    columnsValues.push(new Array())
   }
-  var rowValues = row.join("").replaceAll("false", " ")
-  rowValues = rowValues.split(" ")
-  rowValues.forEach((filledInSection, counter) => {
-    var length = (filledInSection.match(/true/g) || []).length
-    if (length != 0) rowValues[counter] = length;
-  })
-  var value = rowValues.join().replaceAll(",", " ").trim()
-  values.rows[i] = value != "" ? value : "0"
+
+  /* GENERATE Values */
+  for (var i = 0; i < matrix.length; i++) {
+    var row = matrix[i];
+    for (var j = 0; j < row.length; j++) {
+      row[j] = (Math.floor(Math.random() * 24) + 1) % 2 != 0 ? true : false; // Generate value for current block
+      columnsValues[j][i] = row[j]
+    }
+    var rowValues = row.join("").replaceAll("false", " ") // Generate filled in block count for current row
+    rowValues = rowValues.split(" ")
+    rowValues.forEach((filledInSection, counter) => {
+      var length = (filledInSection.match(/true/g) || []).length
+      if (length != 0) rowValues[counter] = length;
+    })
+    var value = rowValues.join().replaceAll(",", " ").trim()
+    values.rows[i] = value != "" ? value : "0"
+  }
+
+  for (var i = 0; i < columnsValues.length; i++) { // Loop through columns and generate filled in block count
+    var column = columnsValues[i];
+    var columnValues = column.join("").replaceAll("false", " ")
+    columnValues = columnValues.split(" ")
+    columnValues.forEach((filledInSection, counter) => {
+      var length = (filledInSection.match(/true/g) || []).length
+      if (length != 0) columnValues[counter] = length;
+    })
+    var value = columnValues.join().replaceAll(",", " ").trim().replaceAll(" ", "<br/>");
+    value = value.replace(/(<br\/>\s*)+/g, '<br/>');
+    values.columns[i] = value != "" ? value : "0"
+  }
+
+  console.log(matrix);
+  console.log(values);
 }
 
-for (var i = 0; i < columns.length; i++) {
-  var column = columns[i];
-  var columnValues = column.join("").replaceAll("false", " ")
-  columnValues = columnValues.split(" ")
-  columnValues.forEach((filledInSection, counter) => {
-    var length = (filledInSection.match(/true/g) || []).length
-    if (length != 0) columnValues[counter] = length;
-  })
-  var value = columnValues.join().replaceAll(",", " ").trim().replaceAll(" ", "<br/>");
-  value = value.replace(/(<br\/>\s*)+/g, '<br/>');
-  values.columns[i] = value != "" ? value : "0"
-}
-
-function check() {
+const check = () => {
   var correct = true
   for (var i = 0; i < matrix.length; i++) {
     var row = matrix[i];
     for (var j = 0; j < row.length; j++) {
-        var block = document.getElementById(`${i+1}x${j+1}`).classList.contains("bg-green-300");
-        var selected = matrix[i][j];
+      var block = document.getElementById(`${i+1}x${j+1}`).classList.contains("bg-green-300");
+      var selected = matrix[i][j];
 
-        if(selected && block){ // If it is supposed to be selected and is
-          continue
-        }
-        else if(!selected && block){ // If it isn't supposed to be and is
-          correct = false;
-          return correct;
-        }
-        else if(selected && !block){ // If it is suppsoed to be and it isn't
-          correct = false;
-          return correct;
-        }
+      if (selected && block) { // If it is supposed to be selected and is
+        continue
+      }
+      else if (!selected && block) { // If it isn't supposed to be and is
+        correct = false;
+        return correct;
+      }
+      else if (selected && !block) { // If it is suppsoed to be and it isn't
+        correct = false;
+        return correct;
+      }
     }
   }
   return correct
 }
 
-function clicked(blockID) {
-  if(notStarted){
-    notStarted = false;
+const clicked =  (blockID) => {
+  if (!gameStarted) {
+    gameStarted = true;
     timer.start()
   }
   var block = document.getElementById(blockID)
@@ -86,15 +102,29 @@ function clicked(blockID) {
   else {
     block.classList.add("bg-green-300")
   }
-  if(check()){
+  if (check()) {
     timer.stop()
-    setTimeout(function(){document.getElementById("modal").classList.remove("hidden")}, 500)
+    setTimeout(function() {
+      document.getElementById("modal").classList.remove("hidden")
+    }, 500)
   }
 }
 
-function restart(){
+const restart = () => {
   document.getElementById("modal").classList.add("hidden");
+  matrix = undefined;
+  values.rows = undefined;
+  values.columns = undefined;
+  timer = undefined;
+  columnsValues = undefined;
+  gameStarted = undefined;
+  init(rows, columns);
+  document.querySelectorAll(".gameBlock").forEach(block => {
+    block.classList.remove("selected", "selected2", "bg-red-500", "bg-green-300")
+  })
 }
+
+init(rows, columns);
 </script>
 
 <template>
@@ -104,13 +134,13 @@ function restart(){
      <thead>
        <tr>
          <td></td>
-         <td class="font-bold text-center" v-for="f in columnCount" v-html="values.columns[f - 1]"></td>
+         <td class="font-bold text-center" v-for="f in columns" v-html="values.columns[f - 1]"></td>
        </tr>
      </thead>
      <tbody>
-       <tr v-for="n in rowCount">
+       <tr v-for="n in rows">
          <td class="font-bold text-center">{{ values.rows[n - 1] }}</td>
-         <td class="border border-black w-[50px] h-[50px]" :id="`${n}x${f}`" v-for="f in columnCount" @click="clicked(`${n}x${f}`)"></td>
+         <td class="border border-black w-[50px] h-[50px] gameBlock" :id="`${n}x${f}`" v-for="f in columns" @click="clicked(`${n}x${f}`)"></td>
        </tr>
      </tbody>
    </table>
@@ -128,13 +158,9 @@ function restart(){
          <p class="text-sm text-gray-500">Nonogram solved in: {{ new Date(timer.getTime()).getSeconds() }} Seconds</p>
        </div>
        <div class="items-center px-4 py-3">
-         <button @click="restart()" class="px-4 py-2 bg-green-500 text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-300"> Restart (doesn't yet work)</button>
+         <button @click="restart()" class="px-4 py-2 bg-green-500 text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-300"> Restart </button>
        </div>
      </div>
    </div>
  </div>
 </template>
-
-<style scoped>
-
-</style>
